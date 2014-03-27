b9b5b697a6fac23de64605dcb6c4d8b339259262
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2014-03-28 00:40:20 +0200

    font-awesome fonts copied to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,9 +285,8 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
-            //'bower_components/**/*',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'bower_components/font-awesome/fonts/*',
           ]
         }, {
           expand: true,
@@ -357,94 +356,95 @@ module.exports = function (grunt) {
     // aws: grunt.file.readJSON('~/grunt-aws.json'),
     // If you do not pass in a key and secret with your config,
     // grunt-s3 will fallback to the following environment variables
+
     s3: {
-      options: {
-        // key: '<%= aws.key %>',
-        // secret: '<%= aws.secret %>',
-        bucket: 'fitoera.com',
-        access: 'public-read',
-        headers: {
-          // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
-          "Cache-Control": "max-age=630720000, public",
-          "Expires": new Date(Date.now() + 63072000000).toUTCString()
-        }
-      },
-      dev: {
-        // These options override the defaults
-        options: {
-          encodePaths: true,
-          maxOperations: 20
-        },
-        // Files to be uploaded.
-        upload: [
-          {
-            src: 'dist/**/*',
-            dest: '**/*',
-            options: { gzip: true }
-          },
-          {
-            src: 'passwords.txt',
-            dest: 'documents/ignore.txt',
+      // options: {
+      //   // key: '<%= aws.key %>',
+      //   // secret: '<%= aws.secret %>',
+      //   bucket: 'fitoera.com',
+      //   access: 'public-read',
+      //   headers: {
+      //     // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+      //     "Cache-Control": "max-age=630720000, public",
+      //     "Expires": new Date(Date.now() + 63072000000).toUTCString()
+      //   }
+      // },
+      // dev: {
+      //   // These options override the defaults
+      //   options: {
+      //     encodePaths: true,
+      //     maxOperations: 20
+      //   },
+      //   // Files to be uploaded.
+      //   upload: [
+      //     {
+      //       src: 'dist/**/*',
+      //       dest: '**/*',
+      //       options: { gzip: true }
+      //     },
+      //     {
+      //       src: 'passwords.txt',
+      //       dest: 'documents/ignore.txt',
 
-            // These values will override the above settings.
-            options: {
-              bucket: 'some-specific-bucket',
-              access: 'authenticated-read'
-            }
-          },
-          {
-            // Wildcards are valid *for uploads only* until I figure out a good implementation
-            // for downloads.
-            src: 'documents/*.txt',
+      //       // These values will override the above settings.
+      //       options: {
+      //         bucket: 'some-specific-bucket',
+      //         access: 'authenticated-read'
+      //       }
+      //     },
+      //     {
+      //       // Wildcards are valid *for uploads only* until I figure out a good implementation
+      //       // for downloads.
+      //       src: 'documents/*.txt',
 
-            // But if you use wildcards, make sure your destination is a directory.
-            dest: 'documents/'
-          }
-        ],
+      //       // But if you use wildcards, make sure your destination is a directory.
+      //       dest: 'documents/'
+      //     }
+      //   ],
 
-        // Files to be downloaded.
-        download: [
-          {
-            src: 'documents/important.txt',
-            dest: 'important_document_download.txt'
-          },
-          {
-            src: 'garbage/IGNORE.txt',
-            dest: 'passwords_download.txt'
-          }
-        ],
+      //   // Files to be downloaded.
+      //   download: [
+      //     {
+      //       src: 'documents/important.txt',
+      //       dest: 'important_document_download.txt'
+      //     },
+      //     {
+      //       src: 'garbage/IGNORE.txt',
+      //       dest: 'passwords_download.txt'
+      //     }
+      //   ],
 
-        del: [
-          {
-            src: 'documents/launch_codes.txt'
-          },
-          {
-            src: 'documents/backup_plan.txt'
-          }
-        ],
+      //   del: [
+      //     {
+      //       src: 'documents/launch_codes.txt'
+      //     },
+      //     {
+      //       src: 'documents/backup_plan.txt'
+      //     }
+      //   ],
 
-        sync: [
-          {
-            // only upload this document if it does not exist already
-            src: 'important_document.txt',
-            dest: 'documents/important.txt',
-            options: { gzip: true }
-          },
-          {
-            // make sure this document is newer than the one on S3 and replace it
-            options: { verify: true },
-            src: 'passwords.txt',
-            dest: 'documents/ignore.txt'
-          },
-          {
-            src: path.join(variable.to.release, "build/cdn/js/**/*.js"),
-            dest: "jsgz",
-            // make sure the wildcard paths are fully expanded in the dest
-            rel: path.join(variable.to.release, "build/cdn/js"),
-            options: { gzip: true }
-          }
-        ]
-      }
+      //   sync: [
+      //     {
+      //       // only upload this document if it does not exist already
+      //       src: 'important_document.txt',
+      //       dest: 'documents/important.txt',
+      //       options: { gzip: true }
+      //     },
+      //     {
+      //       // make sure this document is newer than the one on S3 and replace it
+      //       options: { verify: true },
+      //       src: 'passwords.txt',
+      //       dest: 'documents/ignore.txt'
+      //     },
+      //     {
+      //       src: path.join(variable.to.release, "build/cdn/js/**/*.js"),
+      //       dest: "jsgz",
+      //       // make sure the wildcard paths are fully expanded in the dest
+      //       rel: path.join(variable.to.release, "build/cdn/js"),
+      //       options: { gzip: true }
+      //     }
+      //   ]
+      // }
     }
   });
 
