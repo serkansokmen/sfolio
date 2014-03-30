5314da859b7dedfbab54f32782f36a9ff6385284
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2014-03-30 16:17:33 +0300

    Powered by Behance

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -3,12 +3,15 @@
 angular.module('sfolioApp')
   .controller('ProjectCtrl', function ($scope, $routeParams, $sce, Behance) {
 
+    $scope.loading = true;
     $scope.project = Behance.getProject($routeParams.id)
       .then(function (project) {
         $scope.project = project;
+        $scope.loading = false;
       }, function (error) {
         console.log(error);
-        $scope.project = {};
+        $scope.loading = false;
+        $scope.project = null;
       });
 
     $scope.getModuleEmbed = function (src) {
